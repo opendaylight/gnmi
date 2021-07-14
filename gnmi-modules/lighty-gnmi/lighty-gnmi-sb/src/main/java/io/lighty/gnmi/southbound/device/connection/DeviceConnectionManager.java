caladi/lighty

Implement force-capabilities for rewriting device capabilities
 Add the ability to rewrite device capabilities.
 Add the parameter to ConfigurableParameters and
 implement use_models into each GET request.

Signed-off-by: Ivan Caladi <ivan.caladi@pantheon.tech>

lighty-modules/lighty-gnmi/lighty-gnmi-sb/src/main/java/io/lighty/gnmi/southbound/device/connection/DeviceConnectionManager.java
@@ -23,8 +23,10 @@ import io.lighty.gnmi.southbound.requests.utils.GnmiRequestUtils;
 import io.lighty.gnmi.southbound.schema.SchemaContextHolder;
 import io.lighty.gnmi.southbound.schema.impl.SchemaException;
 import io.lighty.gnmi.southbound.timeout.TimeoutUtils;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -112,8 +114,16 @@ public class DeviceConnectionManager implements AutoCloseable {
                             new MissingEncodingException("gNMI Device must support JSON_IETF encoding"));
                 }
 
-                final List<GnmiDeviceCapability> capabilitiesList =
-                    GnmiRequestUtils.fromCapabilitiesResponse(capabilityResponse);
+                final List<GnmiDeviceCapability> capabilitiesList = new ArrayList<>();
+                final Optional<List<Gnmi.ModelData>> forceCapabilities =
+                    deviceConnection.getConfigurableParameters().getForceCapabilities();
+                if (forceCapabilities.isPresent()) {
+                    final Gnmi.CapabilityResponse capabilitiesResponseBuilder =
+                        Gnmi.CapabilityResponse.newBuilder().addAllSupportedModels(forceCapabilities.get()).build();
+                    capabilitiesList.addAll(GnmiRequestUtils.fromCapabilitiesResponse(capabilitiesResponseBuilder));
+                } else {
+                    capabilitiesList.addAll(GnmiRequestUtils.fromCapabilitiesResponse(capabilityResponse));
+                }
                 try {
                     final EffectiveSchemaContext schemaContext = schemaContextHolder.getSchemaContext(capabilitiesList);
                     deviceConnection.setSchemaContext(schemaContext);

lighty-modules/lighty-gnmi/lighty-gnmi-sb/src/main/java/io/lighty/gnmi/southbound/mountpoint/requests/GnmiGetRequestFactoryImpl.java
@@ -55,6 +55,10 @@ public class GnmiGetRequestFactoryImpl implements GnmiGetRequestFactory {
                 requestBuilder.setPrefix(Gnmi.Path.newBuilder()
                     .setTarget(parameters.getPathTarget().get()));
             }
+            if (parameters.getForceCapabilities().isPresent()) {
+                requestBuilder.addAllUseModels(parameters.getForceCapabilities().get());
+            }
+
             final Gnmi.Path gnmiPath = instanceIdentifierToPathCodec.apply(path);
             return requestBuilder
                     .setEncoding(Encoding.JSON_IETF)

opendaylight/gnmi

gnmi-modules/lighty-gnmi/lighty-gnmi-sb/src/main/java/io/lighty/gnmi/southbound/device/connection/DeviceConnectionManager.java
@@ -23,8 +23,10 @@ import io.lighty.gnmi.southbound.requests.utils.GnmiRequestUtils;
 import io.lighty.gnmi.southbound.schema.SchemaContextHolder;
 import io.lighty.gnmi.southbound.schema.impl.SchemaException;
 import io.lighty.gnmi.southbound.timeout.TimeoutUtils;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -112,8 +114,16 @@ public class DeviceConnectionManager implements AutoCloseable {
                             new MissingEncodingException("gNMI Device must support JSON_IETF encoding"));
                 }
 
-                final List<GnmiDeviceCapability> capabilitiesList =
-                    GnmiRequestUtils.fromCapabilitiesResponse(capabilityResponse);
+                final List<GnmiDeviceCapability> capabilitiesList = new ArrayList<>();
+                final Optional<List<Gnmi.ModelData>> forceCapabilities =
+                    deviceConnection.getConfigurableParameters().getModelDataList();
+                if (forceCapabilities.isPresent()) {
+                    final Gnmi.CapabilityResponse capabilitiesResponseBuilder =
+                        Gnmi.CapabilityResponse.newBuilder().addAllSupportedModels(forceCapabilities.get()).build();
+                    capabilitiesList.addAll(GnmiRequestUtils.fromCapabilitiesResponse(capabilitiesResponseBuilder));
+                } else {
+                    capabilitiesList.addAll(GnmiRequestUtils.fromCapabilitiesResponse(capabilityResponse));
+                }
                 try {
                     final EffectiveSchemaContext schemaContext = schemaContextHolder.getSchemaContext(capabilitiesList);
                     deviceConnection.setSchemaContext(schemaContext);

gnmi-modules/lighty-gnmi/lighty-gnmi-sb/src/main/java/io/lighty/gnmi/southbound/mountpoint/requests/GnmiGetRequestFactoryImpl.java
@@ -55,6 +55,10 @@ public class GnmiGetRequestFactoryImpl implements GnmiGetRequestFactory {
                 requestBuilder.setPrefix(Gnmi.Path.newBuilder()
                     .setTarget(parameters.getPathTarget().get()));
             }
+            if (parameters.getModelDataList().isPresent()) {
+                requestBuilder.addAllUseModels(parameters.getModelDataList().get());
+            }
+
             final Gnmi.Path gnmiPath = instanceIdentifierToPathCodec.apply(path);
             return requestBuilder
                     .setEncoding(Encoding.JSON_IETF)