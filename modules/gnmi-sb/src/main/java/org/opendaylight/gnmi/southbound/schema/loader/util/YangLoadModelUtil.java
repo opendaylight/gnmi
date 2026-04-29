2861fe59855c3ac334688d2e71cae8862f895dac
tobias.pobocik <tobias.pobocik@pantheon.tech>
2026-04-29 12:08:27 +0200

    WIP continue integration
    Change-Id: I061ca8bbbda5544b3062dc6448634f73b393fab9
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- modules/gnmi-sb/src/main/java/org/opendaylight/gnmi/southbound/schema/loader/util/YangLoadModelUtil.java
+++ modules/gnmi-sb/src/main/java/org/opendaylight/gnmi/southbound/schema/loader/util/YangLoadModelUtil.java
@@ -10,45 +10,43 @@ package org.opendaylight.gnmi.southbound.schema.loader.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 import org.apache.commons.io.IOUtils;
 import org.opendaylight.yangtools.concepts.SemVer;
-import org.opendaylight.yangtools.openconfig.model.api.OpenConfigStatements;
+import org.opendaylight.yangtools.openconfig.model.api.OpenConfigVersionStatement;
 import org.opendaylight.yangtools.yang.common.Revision;
 import org.opendaylight.yangtools.yang.ir.IRArgument.Single;
 import org.opendaylight.yangtools.yang.ir.IRStatement;
+import org.opendaylight.yangtools.yang.model.api.source.SourceSyntaxException;
 import org.opendaylight.yangtools.yang.model.api.source.YangTextSource;
+import org.opendaylight.yangtools.yang.model.spi.source.YangIRSource;
 import org.opendaylight.yangtools.yang.parser.api.YangSyntaxErrorException;
-import org.opendaylight.yangtools.yang.parser.rfc7950.repo.TextToIRTransformer;
-import org.opendaylight.yangtools.yang.parser.rfc7950.repo.YangIRSourceInfoExtractor;
+import org.opendaylight.yangtools.yang.source.ir.DefaultYangTextToIRSourceTransformer;
 
 public class YangLoadModelUtil {
 
-    private static final String OPENCONFIG_VERSION = OpenConfigStatements.OPENCONFIG_VERSION.getStatementName()
-            .getLocalName();
+    private static final String OPENCONFIG_VERSION = OpenConfigVersionStatement.DEF.simpleName();
     private final Revision modelRevision;
     private final SemVer modelSemVer;
     private final String modelBody;
     private final String modelName;
 
     public YangLoadModelUtil(final YangTextSource yangTextSchemaSource, final InputStream yangTextStream)
-            throws YangSyntaxErrorException, IOException {
-        final var irSchemaSource = TextToIRTransformer.transformText(yangTextSchemaSource);
-        final var semanticVersion = getSemVer(irSchemaSource.statement());
+        throws YangSyntaxErrorException, IOException, SourceSyntaxException {
 
-        final var yangModelDependencyInfo =
-                YangIRSourceInfoExtractor.forYangText(yangTextSchemaSource);
-        // If revision is present in fileName, prefer that
-        this.modelRevision = yangModelDependencyInfo.revisions().stream()
-                .max(Comparator.comparing(revision -> LocalDate.parse(revision.toString(),
-                        DateTimeFormatter.ofPattern("yyyy-MM-dd"))))
-                .orElse(null);
+        // 1. Transform the string into the native AST (Replaces TextToIRTransformer)
+        final YangIRSource irSchemaSource = new DefaultYangTextToIRSourceTransformer()
+            .transformSource(yangTextSchemaSource);
+        final IRStatement rootStatement = irSchemaSource.statement();
+        final Optional<SemVer> semanticVersion = getSemVer(rootStatement);
+
+        this.modelRevision = extractMaxRevision(rootStatement);
         this.modelSemVer = semanticVersion.orElse(null);
         this.modelBody = IOUtils.toString(yangTextStream, StandardCharsets.UTF_8);
-        this.modelName = yangModelDependencyInfo.sourceId().name().getLocalName();
+        this.modelName = yangTextSchemaSource.sourceId().name().getLocalName();
     }
 
     public String getVersionToStore() {
@@ -82,10 +80,34 @@ public class YangLoadModelUtil {
             if (OPENCONFIG_VERSION.equals(substatement.keyword().identifier())) {
                 final var argument = substatement.argument();
                 if (argument instanceof Single) {
-                    return Optional.of(SemVer.valueOf(((Single) argument).string()));
+                    try {
+                        return Optional.of(SemVer.valueOf(((Single) argument).string()));
+                    } catch (IllegalArgumentException e) {
+                        // Ignore malformed semvers
+                    }
                 }
             }
         }
         return Optional.empty();
+    }
+
+    /**
+     * Traverses the root IRStatement to find all "revision" declarations, returning the newest one.
+     */
+    private static Revision extractMaxRevision(final IRStatement rootStatement) {
+        final List<Revision> revisions = new ArrayList<>();
+
+        for (final IRStatement stmt : rootStatement.statements()) {
+            if ("revision".equals(stmt.keyword().identifier())) {
+                final var argument = stmt.argument();
+                if (argument instanceof Single) {
+                    final String revStr = ((Single) argument).string();
+                    Revision.ofNullable(revStr).ifPresent(revisions::add);
+                }
+            }
+        }
+
+        // OpenDaylight's 'Revision' implements Comparable properly, so naturalOrder() finds the most recent date.
+        return revisions.stream().max(Comparator.naturalOrder()).orElse(null);
     }
 }
