51363929c3cdd8e5149f4f54eeee3b6c68a1c538
tobias.pobocik <tobias.pobocik@pantheon.tech>
2026-04-29 10:21:09 +0200

    Remove YangStatementStreamSource
    OpenDaylight Chromium removed YangStatementStreamSource, requiring us
    to migrate to YangTextSource and direct parsing.
    
    Because OpenDaylight now hides the ANTLR parsing implementation
    (DefaultYangTextToIRSourceTransformer) from OSGi, it cannot be
    instantiated directly. Instead:
    1. Inject the public YangTextToIRSourceTransformer SPI into
       GnmiSouthboundModule via OSGi Declarative Services.
    2. Pass this transformer down to SchemaContextHolderImpl and
       YangLoadModelUtil to manually parse the IRSource and extract
       dependencies and openconfig/semantic versions.
    3. Update gnmi-device-simulator to use the new parser factories
       in its standalone EffectiveModelContextBuilder.
    
    https://github.com/opendaylight/yangtools/commit/b141d2b414d51ae032edbaf4b931903b25db8af6#diff-d7c08c096d00da927ec97f71c2179e2e4649ef31f9811086f9a3bc8fbdb4e1c1
    
    JIRA: GNMI-17
    Change-Id: I63e18778e34c9a51512ebed16aff7ad4f622ce79
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- modules/gnmi-sb/src/main/java/org/opendaylight/gnmi/southbound/schema/loader/impl/ByClassPathYangLoaderService.java
+++ modules/gnmi-sb/src/main/java/org/opendaylight/gnmi/southbound/schema/loader/impl/ByClassPathYangLoaderService.java
@@ -25,7 +25,9 @@ import org.opendaylight.gnmi.southbound.schema.yangstore.service.YangDataStoreSe
 import org.opendaylight.gnmi.southbound.timeout.TimeoutUtils;
 import org.opendaylight.yangtools.binding.meta.YangModuleInfo;
 import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.api.source.SourceSyntaxException;
 import org.opendaylight.yangtools.yang.model.spi.source.DelegatedYangTextSource;
+import org.opendaylight.yangtools.yang.model.spi.source.YangTextToIRSourceTransformer;
 import org.opendaylight.yangtools.yang.parser.api.YangParserFactory;
 import org.opendaylight.yangtools.yang.parser.api.YangSyntaxErrorException;
 import org.slf4j.Logger;
@@ -37,10 +39,13 @@ public class ByClassPathYangLoaderService implements YangLoaderService {
 
     private final Set<YangModuleInfo> yangModulesInfo;
     private final YangParserFactory yangParser;
+    private final YangTextToIRSourceTransformer textToIrTransformer;
 
-    public ByClassPathYangLoaderService(final Set<YangModuleInfo> yangModulesInfo, final YangParserFactory yangParser) {
+    public ByClassPathYangLoaderService(final Set<YangModuleInfo> yangModulesInfo, final YangParserFactory yangParser,
+                                        final YangTextToIRSourceTransformer textToIrTransformer) {
         this.yangModulesInfo = Objects.requireNonNull(yangModulesInfo);
         this.yangParser = Objects.requireNonNull(yangParser);
+        this.textToIrTransformer = Objects.requireNonNull(textToIrTransformer);
     }
 
     @Override
@@ -55,7 +60,8 @@ public class ByClassPathYangLoaderService implements YangLoaderService {
             try (InputStream yangTextStream = yangModuleInfo.openYangTextStream()) {
                 this.yangParser.createParser().addSource(yangTextSchemaSource);
 
-                final YangLoadModelUtil yangLoadModelUtil = new YangLoadModelUtil(yangTextSchemaSource, yangTextStream);
+                final YangLoadModelUtil yangLoadModelUtil = new YangLoadModelUtil(yangTextSchemaSource, yangTextStream,
+                    textToIrTransformer);
                 storeService.addYangModel(yangLoadModelUtil.getModelName(), yangLoadModelUtil.getVersionToStore(),
                         yangLoadModelUtil.getModelBody())
                     .get(TimeoutUtils.DATASTORE_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
@@ -64,7 +70,8 @@ public class ByClassPathYangLoaderService implements YangLoaderService {
                     yangLoadModelUtil.getModelSemVer(), yangLoadModelUtil.getModelRevision()));
                 LOG.info("Loaded yang model {} with version {}", yangLoadModelUtil.getModelName(),
                     yangLoadModelUtil.getVersionToStore());
-            } catch (YangSyntaxErrorException | ExecutionException | TimeoutException | IOException e) {
+            } catch (YangSyntaxErrorException | ExecutionException | TimeoutException | IOException
+                    | SourceSyntaxException e) {
                 throw new YangLoadException(
                     String.format("Loading YangModuleInfo [%s] failed!", yangModuleInfo.getName()), e);
             } catch (InterruptedException e) {
